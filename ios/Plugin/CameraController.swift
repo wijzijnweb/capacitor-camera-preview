780e24554e67f5226e89aff70025b9e3595f8845
Tim van Uum <tim@exapps.nl>
2025-05-02 10:29:53 +0200

    iOS video fix

--- ios/Plugin/CameraController.swift
+++ ios/Plugin/CameraController.swift
@@ -432,7 +432,7 @@ extension CameraController {
 
     }
 
-    func captureVideo() throws {
+     func captureVideo(completion: @escaping (URL?, Error?) -> Void) throws {
         guard let captureSession = self.captureSession, captureSession.isRunning else {
             throw CameraControllerError.captureSessionIsMissing
         }
